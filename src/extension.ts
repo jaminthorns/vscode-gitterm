24cbdd303ab808ce7079470768dad7b013fe759c
Jamin Thornsberry <jamin.thorns@gmail.com>
2023-07-15 03:16:02 -0500

    Implement commit link options

--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,74 @@
 import * as vscode from "vscode";
 import { basename } from "path";
+import { exec } from "child_process";
 
-interface CustomTerminalLink<Context> extends vscode.TerminalLink {
-  context: Context;
+interface TerminalOptionsWithContext extends vscode.TerminalOptions {
+  context: any;
 }
 
-interface CustomTerminalOptions<Context> extends vscode.TerminalOptions {
-  context: Context;
+interface CommitTerminalLink extends vscode.TerminalLink {
+  context: {
+    commit: string;
+    path: string | null;
+  };
 }
-
-interface FileContext {
-  path: string;
-}
-
-interface FileRangeContext extends FileContext {
-  startLine: number;
-  endLine: number;
-}
-
-interface CommitContext {
-  commit: string;
-  path: string | null;
-}
-
-const USE_ACTIVE_TERMINAL = false;
-const OPEN_TERMINAL_MESSAGE =
-  "You must have a terminal open to use this command.";
 
 function isCustomTerminalOptions(
   options: vscode.TerminalOptions
-): options is CustomTerminalOptions<any> {
+): options is TerminalOptionsWithContext {
   return "context" in options;
 }
 
-function getTerminal<Context>(name: string, icon: string, context: Context) {
-  const activeTerminal = vscode.window.activeTerminal;
+async function runCommand(command: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const folders = vscode.workspace.workspaceFolders;
 
-  if (USE_ACTIVE_TERMINAL && activeTerminal === undefined) {
-    vscode.window.showInformationMessage(OPEN_TERMINAL_MESSAGE);
-    return null;
-  } else if (USE_ACTIVE_TERMINAL && activeTerminal !== undefined) {
-    return activeTerminal;
-  } else {
-    const iconPath = new vscode.ThemeIcon(icon);
-    const options: CustomTerminalOptions<Context> = { name, iconPath, context };
+    if (folders !== undefined && folders[0]) {
+      const folder = folders[0];
 
-    return vscode.window.createTerminal(options);
+      exec(`cd ${folder.uri.fsPath} && ${command}`, (error, stdout, stderr) => {
+        if (error === null) {
+          resolve(stdout.trim());
+        } else {
+          reject(stderr.trim());
+        }
+      });
+    } else {
+      reject("No folders are opened in this workspace");
+    }
+  });
+}
+
+async function isCommit(text: string): Promise<boolean> {
+  try {
+    await runCommand(`git rev-parse --verify ${text}`);
+    return true;
+  } catch (error) {
+    return false;
   }
+}
+
+async function abbreviateCommit(commit: string): Promise<string> {
+  return await runCommand(`git rev-parse --short ${commit}`);
+}
+
+function runCommandInTerminal<Context>({
+  name,
+  icon,
+  context,
+  command,
+}: {
+  name: string;
+  icon: string;
+  context: Context;
+  command: string;
+}) {
+  const iconPath = new vscode.ThemeIcon(icon);
+  const options: TerminalOptionsWithContext = { name, iconPath, context };
+  const terminal = vscode.window.createTerminal(options);
+
+  terminal.show();
+  terminal.sendText(command);
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -54,94 +76,130 @@ export function activate(context: vscode.ExtensionContext) {
     "git-terminal-history.fileHistory",
     ({ document }: vscode.TextEditor) => {
       const path = vscode.workspace.asRelativePath(document.uri);
-
       const file = basename(path);
-      const title = `File History: ${file}`;
-      const terminal = getTerminal(title, "history", { path });
 
-      if (terminal === null) {
-        return;
-      }
-
-      const commandText = `git history -f ${path}`;
-
-      terminal.show();
-      terminal.sendText(commandText);
+      runCommandInTerminal({
+        name: `File History: ${file}`,
+        icon: "history",
+        command: `git history -f ${path}`,
+        context: { path },
+      });
     }
   );
 
   const lineHistory = vscode.commands.registerTextEditorCommand(
     "git-terminal-history.lineHistory",
     ({ selection, document }: vscode.TextEditor) => {
+      const startLine = selection.start.line + 1;
+      const endLine = selection.end.line + 1;
+      const lineRange = `${startLine},${endLine}`;
+      const lineSuffix = startLine === endLine ? startLine : lineRange;
+
       const path = vscode.workspace.asRelativePath(document.uri);
-      const context: FileRangeContext = {
-        path,
-        startLine: selection.start.line + 1,
-        endLine: selection.end.line + 1,
-      };
-
       const file = basename(path);
-      const lineRange = `${context.startLine},${context.endLine}`;
-      const title = `Line History: ${file}:${lineRange}`;
-      const terminal = getTerminal(title, "history", context);
 
-      if (terminal === null) {
-        return;
-      }
-
-      const commandText = `git log -L ${lineRange}:${path}`;
-
-      terminal.show();
-      terminal.sendText(commandText);
+      runCommandInTerminal({
+        name: `Line History: ${file}:${lineSuffix}`,
+        icon: "history",
+        command: `git log -L ${lineRange}:${path}`,
+        context: { path },
+      });
     }
   );
 
   const commitLinkProvider = vscode.window.registerTerminalLinkProvider({
-    provideTerminalLinks({
+    async provideTerminalLinks({
       line,
       terminal,
-    }): CustomTerminalLink<CommitContext>[] {
+    }): Promise<CommitTerminalLink[]> {
       const options = terminal.creationOptions;
       const path = isCustomTerminalOptions(options)
         ? options.context.path ?? null
         : null;
 
-      // TODO: Verify commits are real to avoid false positives using `git
-      // rev-parse --verify`.
-      const commitMatches = Array.from(line.matchAll(/([0-9a-f]{7,40})/g));
+      const lineMatches = Array.from(line.matchAll(/([0-9a-f]{7,40})/g));
 
-      return commitMatches.map(([match, commit]) => {
-        const matchStart = line.indexOf(match);
-        const startIndex = matchStart + match.indexOf(commit);
+      const possibleMatches = await Promise.all(
+        lineMatches.map(async ([match, commit]) => ({
+          match,
+          commit,
+          valid: await isCommit(commit),
+        }))
+      );
 
-        return {
-          startIndex,
-          length: commit.length,
-          tooltip: "Open commit in a new terminal",
-          context: { commit, path },
-        };
-      });
+      return possibleMatches
+        .filter(({ valid }) => valid)
+        .map(({ match, commit }) => {
+          const matchStart = line.indexOf(match);
+          const startIndex = matchStart + match.indexOf(commit);
+
+          return {
+            startIndex,
+            length: commit.length,
+            tooltip: "Open commit in a new terminal",
+            context: { commit, path },
+          };
+        });
     },
 
-    handleTerminalLink({ context }: CustomTerminalLink<CommitContext>) {
-      const { commit, path } = context;
+    async handleTerminalLink({ context }: CommitTerminalLink) {
+      const { commit: rawCommit, path } = context;
+      const commit = await abbreviateCommit(rawCommit);
 
-      // TODO: Show quick pick options:
-      // - Show full commit diff
-      // - Show commit diff for file (when file is available through context)
-      // - Show file at commit (when file is available through context)
-      const object = path === null ? commit : `${commit}:${path}`;
-      const title = `Commit: ${object}`;
-      const terminal = getTerminal(title, "git-commit", context);
+      if (path === null) {
+        runCommandInTerminal({
+          name: `Commit: ${commit}`,
+          icon: "git-commit",
+          command: `git show ${commit}`,
+          context: { commit },
+        });
+      } else {
+        const file = basename(path);
 
-      if (terminal === null) {
-        return;
+        const item = await vscode.window.showQuickPick(
+          [
+            {
+              label: "Show Commit",
+              description: commit,
+              onSelected: () => {
+                runCommandInTerminal({
+                  name: `Commit: ${commit}`,
+                  icon: "git-commit",
+                  command: `git show ${commit}`,
+                  context: { commit },
+                });
+              },
+            },
+            {
+              label: "Show File Diff",
+              description: `${file} (${commit})`,
+              onSelected: () => {
+                runCommandInTerminal({
+                  name: `Diff: ${file} (${commit})`,
+                  icon: "git-compare",
+                  command: `git show ${commit} -- ${path}`,
+                  context: { commit, path },
+                });
+              },
+            },
+            {
+              label: "Show File at Commit",
+              description: `${file} (${commit})`,
+              onSelected: () => {
+                runCommandInTerminal({
+                  name: `File: ${file} (${commit})`,
+                  icon: "file",
+                  command: `git show ${commit}:${path}`,
+                  context: { commit, path },
+                });
+              },
+            },
+          ],
+          { placeHolder: "Choose a commit action..." }
+        );
+
+        item?.onSelected();
       }
-
-      const commandText = `git show ${object}`;
-
-      terminal.show();
-      terminal.sendText(commandText);
     },
   });
 
